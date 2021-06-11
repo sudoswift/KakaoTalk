4ccb8c29457c81bc4f7c0d7a144469b3cb592891
sudoswift <sudoswift@gmail.com>
2021-06-11 23:15:36 +0900

    add TabView

--- KakaoTalk/ContentView.swift
+++ KakaoTalk/ContentView.swift
@@ -9,32 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView{
-            Text("some text")
-                .toolbar{
-                    ToolbarItemGroup(placement: .navigationBarLeading){
-                        Text("친구")
-                            .font(.title)
-                    } // ToolbarItemGroup Leading
-                    ToolbarItemGroup(placement: .navigationBarTrailing){
-                        NavigationLink(
-                            destination: Text("Destination"),
-                            label: {
-                                Image(systemName: "magnifyingglass")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .padding(.trailing, 6.0)
-                                    .foregroundColor(.black)
-                            })
-                        Image(systemName: "person.badge.plus")
-                            .padding(.trailing, 6.0)
-                        Image(systemName: "music.note")
-                            .padding(.trailing, 6.0)
-                        Image(systemName: "gearshape")
-                    } // ToolbarItemGroup Trailing
-                } // toolbar
-        } // NavigationView
-        .accentColor(.black) //NavigationView에 accentColor를 적용해주면 toolbar의 색을 지정해줄 수 있다.
+        TabView{
+            NavigationView{
+                Text("some text")
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarLeading){
+                            Text("친구")
+                                .font(.title)
+                        } // ToolbarItemGroup Leading
+                        ToolbarItem(placement: .navigationBarTrailing){
+                            HStack{
+                                Spacer()
+                                NavigationLink(
+                                    destination: Text("Destination"),
+                                    label: {
+                                        Image(systemName: "magnifyingglass")
+                                            .padding(.trailing, 5.0)
+                                            .font(.title2)
+                                    })
+                                NavigationLink(
+                                    destination: Text("Destination"),
+                                    label: {
+                                        Image(systemName: "person.badge.plus")
+                                            .padding(.trailing, 5.0)
+                                            .font(.title2)
+                                    })
+                                NavigationLink(
+                                    destination: Text("Destination"),
+                                    label: {
+                                        Image(systemName: "music.note")
+                                            .padding(.trailing, 5.0)
+                                            .font(.title2)
+                                    })
+                                NavigationLink(
+                                    destination: Text("Destination"),
+                                    label: {
+                                        Image(systemName: "gearshape")
+                                            .font(.title2)
+                                    })
+                            }
+                        } // ToolbarItemGroup Trailing
+                    } // toolbar
+            } // NavigationView
+            .accentColor(.black) //NavigationView에 accentColor를 적용해주면 toolbar의 색을 지정해줄 수 있다.
+        } // TabView
     }
 }
 
