b5e7691255d2b6807c1eff4e9e85f083d97c9406
sudoswift <sudoswift@gmail.com>
2021-06-11 16:39:39 +0900

    Initial Commit

---
+++ KakaoTalk/KakaoTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  KakaoTalkApp.swift
+//  KakaoTalk
+//
+//  Created by izvern on 2021/06/11.
+//
+
+import SwiftUI
+
+@main
+struct KakaoTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
