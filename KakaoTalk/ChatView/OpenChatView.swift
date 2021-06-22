a86e99c5ee90cca99002726b52b6462010046c89
sudoswift <sudoswift@gmail.com>
2021-06-22 13:36:42 +0900

    switch 문을 활용해 ChatView에서 각각 채팅과 오픈채팅을 ontapGesture하게 되면 각각의 View로 넘어가는 기능을 구현했다.

--- KakaoTalk/ChatView/OpenChatView.swift
+++ KakaoTalk/ChatView/OpenChatView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct OpenChatView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            OpenChatList()
+        }
+        .listStyle(SidebarListStyle())
+        .navigationTitle("")
     }
 }
 
--- KakaoTalk/ChatView/PageTwo.swift
+++ KakaoTalk/ChatView/PageTwo.swift
@@ -7,21 +7,53 @@
 
 import SwiftUI
 
-struct PageTwo: View {    
+struct PageTwo: View {
+    
+    @State private var isChat: Bool = true
+    
     var body: some View {
         NavigationView{
-            ChatView()
-                .toolbar{
-                    ToolbarItemGroup(placement: .navigationBarLeading){
-                        Text("채팅").font(.title)
-                        Text("오픈채팅").font(.title).foregroundColor(.secondary)
-                    } //ToolbarItemGroup Leading
-                    ToolbarItemGroup(placement: .navigationBarTrailing){
-                        ChatViewToolbar()
-                    } // ToolbarItemGroup Trailing
-                } // FreindView's Toolbar
-            .navigationBarTitle("", displayMode: .inline)
-            .navigationBarHidden(false)
+            switch isChat {
+            case false :
+                OpenChatView()
+                    .toolbar{
+                        ToolbarItemGroup(placement: .navigationBarLeading){
+                            Text("채팅").font(.title)
+                                .foregroundColor(.secondary)
+                                .onTapGesture(perform: {
+                                    isChat = true
+                                })
+                            Text("오픈채팅").font(.title)
+                                .onTapGesture(perform: {
+                                    isChat = false
+                                })
+                        } //ToolbarItemGroup Leading
+                        ToolbarItemGroup(placement: .navigationBarTrailing){
+                            ChatViewToolbar()
+                        } // ToolbarItemGroup Trailing
+                    } // FreindView's Toolbar
+                    .navigationBarTitle("", displayMode: .inline)
+                    .navigationBarHidden(false)
+            default :
+                ChatView()
+                    .toolbar{
+                        ToolbarItemGroup(placement: .navigationBarLeading){
+                            Text("채팅").font(.title)
+                                .onTapGesture(perform: {
+                                    isChat = true
+                                })
+                            Text("오픈채팅").font(.title).foregroundColor(.secondary)
+                                .onTapGesture(perform: {
+                                    isChat = false
+                                })
+                        } //ToolbarItemGroup Leading
+                        ToolbarItemGroup(placement: .navigationBarTrailing){
+                            ChatViewToolbar()
+                        } // ToolbarItemGroup Trailing
+                    } // FreindView's Toolbar
+                    .navigationBarTitle("", displayMode: .inline)
+                    .navigationBarHidden(false)
+            } // Switch 문
         } // Navigation View 2
         .tabItem {
             Image(systemName: "message")
